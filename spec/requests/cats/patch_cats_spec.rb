af81a0a7a47f01dd559b5a0c8870ce780102932a
Reid Ashwill <reidashwill@gmail.com>
2020-06-12 09:54:52 -0700

    add tests for deleting cats. all dog tests to come next

---
+++ spec/requests/cats/patch_cats_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe 'update cat route' do
+  it 'will update a cat and return correct message' do
+    cat = Cat.create!({name: 'Ms. Purrsimmon', age: "3"})
+    patch "/cats/#{cat.id}", params: {:name => 'Craig'}
+    expect(JSON.parse(response.body)['message']).to eq ("This cat has been successfully updated")
+    expect(response).to have_http_status(200)
+  end
+end
