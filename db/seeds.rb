78de9f20f58de1ab312ed34b29450161fa37ed95
Reid Ashwill <reidashwill@gmail.com>
2020-06-12 08:49:03 -0700

    add seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Cats.destroy_all
+Dogs.destroy_all
+Seed.begin
+p "SUCCESSFULLY SEEDED DB WITH #{Cats.count} CATS AND #{Dogs.count} DOGS"
+class Seed
+
+  def self.begin
+      Seed.generate_cats
+      Seed.generate_dogs
+  end
+
+  def self.generate_cats
+    15.times do |i|
+      cat = Cat.create!(
+        name: Faker::Creature::Cat.name
+        age: rand(1..9)
+        is_available: true
+      )
+    end
+  end
+
+  def self.generate_dogs
+    15.times do |i|
+      dog = Dog.create!(
+        name: Faker::Creature::Dog.name
+        age: rand(1..15)
+        is_available: true
+      )
+    end
+  end
+
+end
