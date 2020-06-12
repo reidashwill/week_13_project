5990861b90333e7fc6c2aebdd1e729a0fea87db7
Reid Ashwill <reidashwill@gmail.com>
2020-06-12 15:53:21 -0700

    refactor all failing tests to include breed

--- spec/requests/dogs/post_dogs_spec.rb
+++ spec/requests/dogs/post_dogs_spec.rb
@@ -1,19 +1,19 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe "post a dog route" do
+describe "post a dog route" do
 
-#   before do
-#     post '/dogs', params: {:name => 'Elvis', :age => '4'}
-#   end
+  before do
+    post '/dogs', params: {:name => 'Elvis', :age => '4', breed: "test"}
+  end
 
-#   it 'returns the dogs information' do
-#     expect(JSON.parse(response.body)['name']).to eq ('Elvis')
-#     expect(JSON.parse(response.body)['age']).to eq(4)
-#   end
+  it 'returns the dogs information' do
+    expect(JSON.parse(response.body)['name']).to eq ('Elvis')
+    expect(JSON.parse(response.body)['age']).to eq(4)
+  end
 
 
-#   it 'returns a created status' do
-#     expect(response).to have_http_status(:created)
-#   end
-# end
+  it 'returns a created status' do
+    expect(response).to have_http_status(:created)
+  end
+end
  
