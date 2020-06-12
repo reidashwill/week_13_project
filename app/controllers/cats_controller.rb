9831f1754469ac987e333170c74574f42274ddb0
Reid Ashwill <reidashwill@gmail.com>
2020-06-12 08:37:22 -0700

    add all basic crud methods to cats_controller and to dogs_controller.

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,5 +1,38 @@
 class CatsController < ApplicationController
 
-  
+  def index
+    @cats = Cats.all
+    json_response(@cats)
+  end
+
+  def show
+    @cat = Cat.find(params[:id])
+    json_response(@cat)
+  end
+
+  def create
+    @cat = Cat.create(cat_params)
+    json_response(@cat)
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+    @cat.update(cat_params)
+  end
+
+  def destroy
+    @cat = Cat.find(params[:id])
+    @cat.destroy
+  end
+
+  private
+
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+
+  def cat_params
+    params.permit(:name, :age, :is_available)
+  end
 
 end
--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,5 +1,38 @@
 class DogsController < ApplicationController
 
-  
+  def index
+    @dogs = Dogs.all
+    json_response(@dogs)
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+    json_response(@dog)
+  end
+
+  def create
+    @dog = Dog.create(dog_params)
+    json_response(@dog)
+  end
+
+  def update
+    @dog = Dog.find(params[:id])
+    @dog.update(dog_params)
+  end
+
+  def destroy
+    @dog = Dog.find(params[:id])
+    @dog.destroy
+  end
+
+  private
+
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+
+  def dog_params
+    params.permit(:name, :age, :is_available)
+  end
 
 end
